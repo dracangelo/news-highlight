05184828fa0a3361c3f8fa5d3bb3aa256f510084
dracangelo <kelvinmbuguaw@gmail.com>
2020-01-07 10:11:21 +0300

    added a def for various news sources

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ def index():
     desc = []
     news = []
     img = []
+    pubAt=[]
+    url= []
+    
 
 
     for i in range (len(articles)):
@@ -27,12 +30,145 @@ def index():
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
 
 
     myList = zip(news, desc, img)
 
 
     return render_template ('index.html', context = myList)
+
+
+ @app.route('/abc')
+def abc():
+    
+    
+    """
+ A view root page function that returns the index page and its data
+    """
+    newsapi = NewsApiClient(api_key="c43cae8199d1435fa1e4cc0737cd4a88")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news-au")
+    
+    articles = topheadlines['articles']
+     
+    des=[]
+    image=[]
+    news=[]
+    pubAt=[]
+    url= []
+    
+  
+    
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        
+        news.append(myarticles['title'])
+        image.append(myarticles['urlToImage'])
+        des.append(myarticles['description'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
+ 
+        
+    mylist=zip(des,image,news,pubAt,url)
+    return render_template('abc.html', context = mylist)
+@app.route('/Bbc')
+def Bbc():
+    
+    
+    """
+ A view root page function that returns the index page and its data
+    """
+    newsapi = NewsApiClient(api_key="c43cae8199d1435fa1e4cc0737cd4a88")
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+    
+    articles = topheadlines['articles']
+     
+    des=[]
+    image=[]
+    news=[]
+    pubAt=[]
+    url= []
+    
+  
+    
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        
+        news.append(myarticles['title'])
+        image.append(myarticles['urlToImage'])
+        des.append(myarticles['description'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
+ 
+        
+    mylist=zip(des,image,news,pubAt,url)
+    return render_template('Bbc.html', context = mylist)
+@app.route('/Cnn')
+def Cnn():
+    
+    
+    """
+ A view root page function that returns the index page and its data
+    """
+    newsapi = NewsApiClient(api_key="c43cae8199d1435fa1e4cc0737cd4a88")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news-au")
+    
+    articles = topheadlines['articles']
+     
+    des=[]
+    image=[]
+    news=[]
+    pubAt=[]
+    url= []
+    
+  
+    
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        
+        news.append(myarticles['title'])
+        image.append(myarticles['urlToImage'])
+        des.append(myarticles['description'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
+ 
+        
+    mylist=zip(des,image,news,pubAt,url)
+    return render_template('Cnn.html', context = mylist)
+@app.route('/Aljazeera')
+def Aljzeera():
+    
+    
+    """
+ A view root page function that returns the index page and its data
+    """
+    newsapi = NewsApiClient(api_key="c43cae8199d1435fa1e4cc0737cd4a88")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news-au")
+    
+    articles = topheadlines['articles']
+     
+    des=[]
+    image=[]
+    news=[]
+    pubAt=[]
+    url= []
+    
+  
+    
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        
+        news.append(myarticles['title'])
+        image.append(myarticles['urlToImage'])
+        des.append(myarticles['description'])
+        pubAt.append(myarticles['publishedAt'])
+        url.append(myarticles['url'])
+ 
+        
+    mylist=zip(des,image,news,pubAt,url)
+    return render_template('Aljzeera.html', context = mylist)
+   
 
 
 
